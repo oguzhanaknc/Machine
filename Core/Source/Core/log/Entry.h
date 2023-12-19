9396a5bacdf2df9828eb8501e29dafc6e8d26dbc
oguzhanaknc <oguzzhanakinci@gmail.com>
2023-12-19 22:07:15 +0300

    fix initalition warns.

--- Core/Source/Core/log/Entry.h
+++ Core/Source/Core/log/Entry.h
@@ -4,11 +4,11 @@
 namespace machine::log {
 	struct Entry
 	{
-		Level level_;
+		Level level_ = Level::Error;
 		std::wstring note_;
-		const wchar_t* sourceFile_;
-		const wchar_t* sourceFunctionName_;
-		int sourceLine_;
+		const wchar_t* sourceFile_ = nullptr;
+		const wchar_t* sourceFunctionName_ = nullptr;
+		int sourceLine_ = -1;
 		std::chrono::system_clock::time_point timeStamp;
 	};
 }
